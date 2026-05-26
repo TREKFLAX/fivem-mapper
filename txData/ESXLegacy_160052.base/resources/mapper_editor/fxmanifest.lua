8706a4faf253a7cad57a06f90a464a5cbfc90fcc
GRINMORD <grinmord@gmail.com>
2026-05-27 00:53:28 +0100

    Primer commit - Subida inicial del proyecto

---
+++ txData/ESXLegacy_160052.base/resources/mapper_editor/fxmanifest.lua
@@ -0,0 +1,18 @@
+fx_version 'cerulean'
+game 'gta5'
+lua54 'yes'
+author 'OpenAI'
+description 'Mapper editor + persistent loader for FiveM'
+version '1.0.0'
+shared_scripts {
+'shared/config.lua'
+}
+client_scripts {
+'client/main.lua'
+}
+server_scripts {
+'server/main.lua'
+}
+files {
+'data/spawns.json'
+}
